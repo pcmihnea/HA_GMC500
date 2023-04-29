c51a3d0cbe1e88538278cbc52792c0884b65344b
pcmihnea <pcmihnea@users.noreply.github.com>
2023-04-29 22:28:13 +0200

    Update mqtt_gmc500_appdaemon.py

--- scripts/mqtt_gmc500_appdaemon.py
+++ scripts/mqtt_gmc500_appdaemon.py
@@ -7,7 +7,6 @@ GMC_DEV_ID = '01234'
 class mqtt_gmc500(mqtt.Mqtt):
     def initialize(self):
         try:
-            self.register_endpoint(self.http_gmc500, "gmc")
             for value in ['CPM', 'ACPM', 'uSV']:
                 self.mqtt_publish('homeassistant/sensor/GMC500_' + value + '/config',
                             payload=json.dumps({
@@ -20,6 +19,14 @@ class mqtt_gmc500(mqtt.Mqtt):
                                 "unique_id": GMC_USER_ID + GMC_DEV_ID + value,
                                 "expire_after": 240}),
                             retain=True)
+            self.register_endpoint(self.http_gmc500, "gmc")
             self.log('Registered.')
         except Exception as e:
             self.log(e)
+
+    def http_gmc500(self, args, cb_args):
+        try:
+            self.mqtt_publish('homeassistant/sensor/GMC500/state', payload=json.dumps({'CPM':args['CPM'], 'ACPM':args['ACPM'], 'uSV':args['uSV']}))
+        except Exception as e:
+            self.log(e)
+        return {"status":"OK"}, 200
